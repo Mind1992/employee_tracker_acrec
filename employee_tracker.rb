3138bb9eca53aabce8414e4556991f5f73d69237
Sergii Mertsalov <student@epicodus.com>
2014-08-12 15:42:38 -0700

    list all the employees

--- employee_tracker.rb
+++ employee_tracker.rb
@@ -9,10 +9,12 @@ def main_menu
   user_input = nil
   until user_input == '0'
     puts "1: add employee"
+    puts "2: list all the employees"
     puts "0: exit"
     user_input = gets.chomp
     case user_input
       when "1" then add_employee
+      when "2" then list_employees
       when "0" then exit
       else
         puts "There is no such command"
@@ -26,6 +28,17 @@ def add_employee
   employee = Employee.new({:name => employee_name})
   employee.save
   puts "* '#{employee_name}' * has been added to the system."
+end
+
+def list_employees
+  puts "\n\n"
+  puts "Here are all the employees: "
+  counter = 1;
+  Employee.all.each do |employee|
+    puts "#{counter.to_s}. #{employee.name}"
+    counter += 1
+  end
+  puts "\n\n"
 end
 
 
